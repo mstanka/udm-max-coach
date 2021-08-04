bd0948e43136061b2c46e45de94761a4d1641c3f
mstanka <mice78.mm@gmail.com>
2021-08-04 12:36:03 +0200

    add coaches to vuex

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,7 +1,7 @@
 <template>
   <base-card>
     <h2>Register as a coach now!</h2>
-    <coach-form></coach-form>
+    <coach-form @save-data="saveData"></coach-form>
   </base-card>
 </template>
 
@@ -11,6 +11,12 @@ import CoachForm from '../../components/coaches/CoachForm.vue';
 export default {
   components: {
     CoachForm,
+  }, 
+  methods: {
+    saveData(data) {
+      this.$store.dispatch('coaches/registerCoach', data);
+      this.$router.replace('/coaches');
+    },
   },
 };
 </script>
