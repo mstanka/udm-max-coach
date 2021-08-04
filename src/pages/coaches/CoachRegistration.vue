2edf5ab12395d91c5f44860f9b8b8eb1fcfdc506
mstanka <mice78.mm@gmail.com>
2021-08-04 10:12:08 +0200

    add coach form

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,16 @@
 <template>
-  register
+  <base-card>
+    <h2>Register as a coach now!</h2>
+    <coach-form></coach-form>
+  </base-card>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+
+export default {
+  components: {
+    CoachForm,
+  },
+};
+</script>
