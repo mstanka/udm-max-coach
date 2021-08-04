eef03e85c0a0eda1dd2b9991d533c90a6d765b63
mstanka <mice78.mm@gmail.com>
2021-08-04 17:26:40 +0200

    connect with Firebase

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,13 +1,58 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
       hourlyRate: data.rate,
       areas: data.areas,
     };
-    context.commit('registerCoach', coachData);
+
+    const response = await fetch(
+      `https://udm-max-coach-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData),
+      },
+    );
+
+    if (!response.ok) {
+      // error...
+    }
+
+    // const responseData = await response.json()
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userIdsa,
+    });
+  },
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://udm-max-coach-default-rtdb.europe-west1.firebasedatabase.app/coaches.json`,
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      // error...
+    }
+
+    const coaches = [];
+
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+
+    context.commit('setCoaches', coaches);
   },
 };
