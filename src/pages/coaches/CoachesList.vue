0c40ee3fc1bcb7e8320c8e1c5b2e2bc11efea8ca
mstanka <mice78.mm@gmail.com>
2021-08-05 12:59:27 +0200

    add http error handling

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,4 +1,8 @@
 <template>
+  <!--  !! converts string truthy value into real Bollean -->
+  <base-dialog :show="!!error" title="An error occured!" @close="handleError">
+    <p>{{ error }}</p>
+  </base-dialog>
   <section>
     <coach-filter @change-filter="setFilters"></coach-filter>
   </section>
@@ -41,6 +45,7 @@ export default {
   data() {
     return {
       isLoading: false,
+      error: null,
       activeFilters: {
         frontend: true,
         backend: true,
@@ -82,8 +87,16 @@ export default {
     async loadCoaches() {
       this.isLoading = true;
       // we are dispatching promise, we can await when its fullfiled and change the loading status
-      await this.$store.dispatch('coaches/loadCoaches');
+      try {
+        await this.$store.dispatch('coaches/loadCoaches');
+      } catch (error) {
+        this.error = error.message || 'Something went wrong!';
+      }
+
       this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
     },
   },
 };
