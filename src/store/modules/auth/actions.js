5f220c17858992bbdcdabc37dfd79d930acae883
mstanka <mice78.mm@gmail.com>
2021-08-06 13:39:06 +0200

    add login functionality

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -29,6 +29,36 @@ export default {
       tokenExpiration: responseData.expiresIn,
     });
   },
+  async login(context, payload) {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBmZ5HVFYrZJ2dtOJqpAQhPF4CrT1RKkBQ',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      },
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      console.log(responseData);
+      const error = new Error(
+        responseData.message || 'Failed to authenticate.',
+      );
+      throw error;
+    }
+
+    console.log(responseData);
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
 };
 
 // API_KEY
@@ -36,3 +66,5 @@ export default {
 
 // Firebase docs
 // https://firebase.google.com/docs/reference/rest/auth/#section-create-email-password
+
+// only url to fetch is different in login and signup...
