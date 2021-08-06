bdeb6b40119c221797edc6e83c578232736cef4e
mstanka <mice78.mm@gmail.com>
2021-08-06 12:49:24 +0200

    add signup functionality

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,1 +1,38 @@
-export default {};
+export default {
+  async signup(context, payload) {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBmZ5HVFYrZJ2dtOJqpAQhPF4CrT1RKkBQ',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      },
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      console.log(responseData);
+      const error = new Error(
+        responseData.message || 'Failed to authenticate.',
+      );
+      throw error;
+    }
+
+    console.log(responseData);
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
+};
+
+// API_KEY
+// see https://stackoverflow.com/questions/37482366/is-it-safe-to-expose-firebase-apikey-to-the-public/37484053#37484053
+
+// Firebase docs
+// https://firebase.google.com/docs/reference/rest/auth/#section-create-email-password
